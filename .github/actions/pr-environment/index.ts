da9a66778604814bece46d7ddfc6357a5bd61a0e
Braden Snell <braden@censys.io>
2020-01-03 16:39:54 -0500

    test

--- .github/actions/pr-environment/index.ts
+++ .github/actions/pr-environment/index.ts
@@ -78,7 +78,7 @@ async function main() {
     repo: "type-route",
     head_sha: sha,
     headers: {
-      "X-GitHub-Media-Type": "application/vnd.github.antiope-preview+json"
+      "X-GitHub-Media-Type": "application/vnd.github.hi-preview+json"
     },
     name: "Hello",
     details_url: `https://codesandbox.io/s/${response.body.sandbox_id}?module=src/playground.tsx`,
