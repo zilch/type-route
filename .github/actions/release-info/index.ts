3f76796cf06ee1c36a41a542235173887f0778e9
Braden Snell <braden@Snells-MacBook-Pro.local>
2020-02-14 23:31:30 -0500

    improves error message

--- .github/actions/release-info/index.ts
+++ .github/actions/release-info/index.ts
@@ -21,7 +21,7 @@ async function main() {
 
   if (refPrefix === null) {
     throw new Error(
-      `Expected ref to match "${refsTagsPrefix}*" or "${refsHeadsPrefix}*"`
+      `Expected ref to match "${refsTagsPrefix}*" or "${refsHeadsPrefix}*" was "${refPrefix}"`
     );
   }
 
