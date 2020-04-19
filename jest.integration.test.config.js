700f9d42b1a26f74b99e85260861fcdb361ca716
Braden Snell <bradenhs@outlook.com>
2020-04-18 22:45:38 -0400

    test

--- jest.integration.test.config.js
+++ jest.integration.test.config.js
@@ -1,12 +1,8 @@
-const MAX_32_BIT_SIGNED_INTEGER = 2147483647;
-
 module.exports = {
   preset: "jest-playwright-preset",
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
   testMatch: ["**/*.integration.test.ts?(x)"],
-  testTimeout: Object.keys(process.env).includes("CI")
-    ? 60000
-    : MAX_32_BIT_SIGNED_INTEGER,
+  testTimeout: 60000,
 };
