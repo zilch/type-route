d2203242f5ec250333094f57cdb38b0580b9c9c2
Braden Snell <bradenhs@outlook.com>
2020-04-18 21:00:19 -0400

    removes unused var

--- src/test/integration/react.integration.test.tsx
+++ src/test/integration/react.integration.test.tsx
@@ -9,7 +9,7 @@ describe("react", () => {
   test("right clicking link", testNotImplemented);
 
   test("react", async () => {
-    const { routes, session } = await page.evaluate(() => {
+    const { routes } = await page.evaluate(() => {
       const { render, TypeRoute, React } = window;
 
       const { createRouter, defineRoute, param } = TypeRoute;
